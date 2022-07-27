37ad7c1b0b029d00fe2913cd9ed739a6af6a9d74
Darren Snee <dsn@pml.ac.uk>
2022-07-27 12:02:00 +0100

    moved interactive mode into its own file

---
+++ run-interactively.sh
@@ -0,0 +1,11 @@
+ docker rm my-first-container;
+
+ docker run \
+		-p 80:8080 \
+		 -it \
+	 	 --name my-first-container \
+		--mount type=bind,source="$(pwd)"/storage,target=/external \
+		--mount type=bind,source="$(pwd)"/test,target=/test \
+		example-app \
+		/bin/bash 
+
--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
  docker rm my-first-container;
 
  docker run \
- -p 80:8080 \
- -it \
+		-p 80:8080 \
  --name my-first-container \
  --mount type=bind,source="$(pwd)"/storage,target=/external \
- --mount type=bind,source="$(pwd)"/test,target=/test \
- example-app /bin/bash \
+ example-app 
+
